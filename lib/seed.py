1928d885e592423ee232c4a511d95b0b38114bb2
Earl <earlmunyua96@gmail.com>
2024-01-10 06:55:03 +0300

    seeding

--- lib/seed.py
+++ lib/seed.py
@@ -5,12 +5,43 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game
+from models import Game, Base
 
 fake = Faker()
 
 if __name__ == '__main__':
     
     engine = create_engine('sqlite:///seed_db.db')
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    botw = Game(title="Breath of the Wild", platform="Switch", genre="Adventure", price=60)
+    ffvii = Game(title="Final Fantasy VII", platform="Playstation", genre="RPG", price=30)
+    mk8 = Game(title="Mario Kart 8", platform="Switch", genre="Racing", price=50)
+    
+    session.bulk_save_objects([botw, ffvii, mk8])
+    session.commit()
+
+    ccs = Game(title="Candy Crush Saga", platform="Mobile", genre="Puzzle", price=0)
+    session.add(ccs)
+    session.commit()
+
+    games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60)
+    )
+
+    for i in range(50)]
+    
+    session.bulk_save_objects(games)
+    session.commit()
+
+    gta = Game(title="GrandtheftAuto 5",platform="Playstation",genre="Action FPS",price=1500)
+    session.add(gta)
+    session.commit()
+
